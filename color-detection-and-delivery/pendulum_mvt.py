4c576f0387cdd4754c4bb86deb88b9c7239b95da
wissamabcc <wissam.abchiche@mail.mcgill.ca>
2025-11-05 16:38:57 -0500

    Update pendulum_mvt.py

--- color-detection-and-delivery/pendulum_mvt.py
+++ color-detection-and-delivery/pendulum_mvt.py
@@ -1,13 +1,20 @@
-from utils.brick import Motor, TouchSensor, BP, wait_ready_sensors, EV3UltrasonicSensor
+from utils.brick import Motor, TouchSensor, BP, wait_ready_sensors, EV3ColorSensor
 from brickpi3 import SensorError
+from color_detection_algorithm import ColorDetectionAlgorithm
 import time
 
 #----------- CONSTANTS -----------#
 INITIAL_POSITION = 0
 LEFT_POSITION = -45
 RIGHT_POSITION = 45
-MOTOR_DPS = 100
+MOTOR_DPS = 50
 TIME_SLEEP = 0.5
+
+#----- Color detection object -----#
+csa = ColorDetectionAlgorithm()
+COLOR_SENSOR = EV3ColorSensor(4)
+counter = 0
+color = None
 
 #------------- SETUP -------------#
 #ULTRASOUND_SENSOR = EV3UltrasonicSensor(2)
@@ -27,7 +34,18 @@ try:
     while True:
         try: 
             #if TOUCH_SENSOR.is_pressed():
-            print("Button pressed!") 
+
+            values = COLOR_SENSOR.get_value()
+        
+            if values:
+                R, G, B, L = values
+                color = csa.classify_the_color(R, G, B)
+                if (color == "green" or color== "red"):
+                    print(color)
+
+                #if(color == "red" or color == "green"):
+                    #break
+                    
             motor.set_dps(MOTOR_DPS)
             motor.set_position(LEFT_POSITION)
             time.sleep(TIME_SLEEP)
@@ -37,6 +55,8 @@ try:
             time.sleep(TIME_SLEEP)
             motor.set_position(INITIAL_POSITION)
             time.sleep(TIME_SLEEP)
+
+        
 
         except SensorError as error:
             print("Sensor error:", error)
