fe0dcb01960ee05dbdbdbe302297ab802567db75
mashaklin <mariaklinski@gmail.com>
2025-11-19 12:37:26 -0500

    added print statements to debug

--- color-detection-and-delivery/pendulum_mvt.py
+++ color-detection-and-delivery/pendulum_mvt.py
@@ -38,7 +38,7 @@ def color_sample():
             if values:
                 R, G, B, L = values
                 color = csa.classify_the_color(R, G, B)
-                print(color)
+                #print(color)
                 if color == "green" or color == "red":
                     return color
                 
@@ -65,21 +65,34 @@ def color_sample():
     return None
 
 def set_position_arms(position):
+    print("setting position arms")
     motor_pendulum.set_position(position)
+    print("setting position arms 1")
     motor_block1.set_position(position)
+    print("setting position arms 2")
     motor_block2.set_position(position)
+    print("setting position arms 3")
 
 def set_dps_arms(dps):
+    print("setting dps arms")
     motor_pendulum.set_dps(dps)
+    print("setting dps arms 1")
     motor_block1.set_dps(dps)
+    print("setting dps arms 2")
     motor_block2.set_dps(dps)
+    print("setting dps arms 3")
 
 def set_power_arms(power):
+    print("setting power arms")
     motor_pendulum.set_dps(power)
+    print("setting power arms 1")
     motor_block1.set_dps(power)
+    print("setting power arms 2")
     motor_block2.set_dps(power)
+    print("setting power arms 3")
 
 def detected_color_algorithm(position, dps, power) :
+    print("setting detected color algo")
     set_position_arms(position)
     time.sleep(1)
     set_dps_arms(dps)
