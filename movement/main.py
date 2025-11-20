ed154e39d41036fea4b33aa086bc2076187b405f
jenni4u <jennifer.you@mail.mcgill.ca>
2025-11-20 14:19:05 -0500

    commit before merge

--- movement/main.py
+++ movement/main.py
@@ -2,9 +2,9 @@ from utils.brick import Motor, wait_ready_sensors, EV3UltrasonicSensor, EV3Color
 import line_follower as lf
 
 # === Initialization ===
-left_motor = Motor("B")
-right_motor = Motor("C")
-color_sensor = EV3ColorSensor(1, mode="red")    
+LEFT_MOTOR = Motor("B")
+RIGHT_MOTOR = Motor("C")
+COLOR_SENSOR = EV3ColorSensor(1, mode="red")    
 
 # INTERSECTION PATTERN
 ROOM = 0        # Meeting Room
@@ -24,24 +24,24 @@ wait_ready_sensors(True)
 
 i = 0
 while i < len(INTERSECTION_PATTERN):
-    lf.line_follower(left_motor, right_motor, color_sensor)
+    lf.line_follower(LEFT_MOTOR, RIGHT_MOTOR, COLOR_SENSOR)
     if INTERSECTION_PATTERN[i] == lf.ROOM:
         print("At meeting room, turning right 90 degrees")
-        lf.simple_move_straight(17.5, left_motor, right_motor, color_sensor)
-        lf.turn_right_on_self(left_motor, right_motor)
-        lf.undo_turn_right_on_self(left_motor, right_motor)
+        lf.simple_move_straight(17.5, LEFT_MOTOR, RIGHT_MOTOR, COLOR_SENSOR)
+        lf.turn_right_on_self(LEFT_MOTOR, RIGHT_MOTOR)
+        lf.undo_turn_right_on_self(LEFT_MOTOR, RIGHT_MOTOR)
     elif INTERSECTION_PATTERN[i] == lf.NEW_EDGE:
         print("At new edge, smooth turning right")
-        lf.simple_move_straight(8.5, left_motor, right_motor, color_sensor)
-        lf.smooth_turn(left_motor, right_motor)
+        lf.simple_move_straight(8.5, LEFT_MOTOR, RIGHT_MOTOR, COLOR_SENSOR)
+        lf.smooth_turn(LEFT_MOTOR, RIGHT_MOTOR, COLOR_SENSOR)
     elif INTERSECTION_PATTERN[i] == lf.ST_ROOM:
         print("At storage room, skipping for now")
         busy_sleep(2)
     i += 1
 
 
-#turn_right_90(left_motor, right_motor, color_sensor)
 
-#line_follower.simple_move_straight(left_motor, right_motor, color_sensor, 17.5)
-#line_follower.turn_right_on_self(left_motor, right_motor)
-#line_follower.undo_turn_right_on_self(left_motor, right_motor)
+
+
+
+
--- movement/return_home.py
+++ movement/return_home.py
@@ -1,5 +1,12 @@
-from utils.brick import Motor, wait_ready_sensors, EV3UltrasonicSensor, EV3ColorSensor, busy_sleep
+from utils.brick import Sound
 import line_follower as lf
+
+def success_sound(sound_file):
+    """Play a sound to indicate successful return."""
+    
+    sound = Sound()
+    sound.play_file(sound_file)
+
 
 def return_home(room):
     """
@@ -13,4 +20,6 @@ def return_home(room):
         # assumes readjustments and alignment are included in the turning function
         lf.turn_right_on_self() 
         lf.line_follower() #TODO: measure distance and add
+        
+        
 
