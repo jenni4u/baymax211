38f62bde9fa0fe4b8beae4c87d7f56ea0e535af5
VICTOIRE BERGERON <victoire.bergeron@mail.mcgill.ca>
2025-11-21 17:49:17 -0500

    fixed storage room turn main

--- movement/main.py
+++ movement/main.py
@@ -1,11 +1,12 @@
-from utils.brick import Motor, wait_ready_sensors, EV3UltrasonicSensor, EV3ColorSensor, busy_sleep
+from utils.brick import Motor, wait_ready_sensors, EV3UltrasonicSensor, EV3ColorSensor, busy_sleep, TouchSensor
 from return_home import return_home
 import line_follower as lf
 
 # === Initialization ===
 left_motor = Motor("B")
 right_motor = Motor("C")
-color_sensor = EV3ColorSensor(3, mode="red")    
+color_sensor = EV3ColorSensor(3, mode="red")  
+touch_sensor = TouchSensor(4)  
 wait_ready_sensors(True)
 
 # INTERSECTION PATTERN
