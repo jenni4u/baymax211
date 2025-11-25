79103e75bd0c424abc1ee4277318aeca1d501b96
DorySong <dorysongmtl@gmail.com>
2025-11-25 14:30:59 -0500

    added emergency stop to return_home.py

--- movement/return_home.py
+++ movement/return_home.py
@@ -6,6 +6,10 @@ def move_to_storage_room():
     Moves robot from intersection to storage room.
     """
     lf.line_follower_distance(15, kp = -1.5)
+    
+    if lf.emergency_stop:
+        return
+    
     lf.move_forward(22)
 
 def return_home(room: int):
@@ -17,14 +21,28 @@ def return_home(room: int):
         # back up
         print("back up 1")
         lf.line_follower(False)
+        
+        if lf.emergency_stop:
+            return
+        
         print("back up 2")
         lf.move_forward(-3)
+        
+        if lf.emergency_stop:
+            return
+        
         print("back up 3")
         lf.line_follower(False)
+        
+        if lf.emergency_stop:
+            return
 
         # turn towards storage room
         # assumes readjustments and alignment are included in the turning function
-        lf.turn_storage_room() 
+        lf.turn_storage_room()
+        
+        if lf.emergency_stop:
+            return
 
         move_to_storage_room()
         
@@ -32,22 +50,41 @@ def return_home(room: int):
     elif room == 3:
         # move forward to corner
         lf.line_follower()
+        
+        if lf.emergency_stop:
+            return
 
         # turning at the top right corner
         lf.smooth_turn()
+        
+        if lf.emergency_stop:
+            return
 
         #move forward to storage room intersection
         lf.line_follower()
+        
+        if lf.emergency_stop:
+            return
 
         # turn towards storage room
         # assumes readjustments and alignment are included in the turning function
         lf.turn_storage_room()
+        
+        if lf.emergency_stop:
+            return
+        
         move_to_storage_room()
     elif room == 1:
         #move forward to storage room intersection
         lf.line_follower()
+        
+        if lf.emergency_stop:
+            return
 
         lf.turn_storage_room()
+        
+        if lf.emergency_stop:
+            return
 
         move_to_storage_room()
 
